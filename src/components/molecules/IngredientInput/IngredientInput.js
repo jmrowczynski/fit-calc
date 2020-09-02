b93eefe79543df19a187279b678798a0e0b559ad
Jakub <jmrowczynski12@gmail.com>
2020-09-02 20:47:26 +0200

    finished appearance of ingredient input

--- src/components/molecules/IngredientInput/IngredientInput.js
+++ src/components/molecules/IngredientInput/IngredientInput.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import Input from 'components/atoms/Input/Input';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
 
+const StyledWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 2fr 1fr 30px 30px;
+    align-items: center;
+    grid-gap: 5px;
+`;
+
 const IgredientInput = () => (
-    <div>
-        <Input secondary />
-        <Input secondary />
+    <StyledWrapper>
+        <Input inputName="ingredient-1" secondary placeholder="Tomato" />
+        <Input inputName="ingredient-1-quantity" secondary placeholder="20g" />
         <ButtonIcon />
         <ButtonIcon plus />
-    </div>
+    </StyledWrapper>
 );
 
 export default IgredientInput;
